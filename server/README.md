4a4c04b59f24e99d6e127b89ae2630dd4e6017a9
mahbubshohag <mahbubislam254@gmail.com>
2025-10-02 02:40:47 +0600

    ok

---
+++ server/README.md
@@ -0,0 +1,12 @@
+# ECommerce project MERN project
+
+1. course plan
+2. Environment setup 
+3. create Express server
+4. Http request response
+5. nodemon and morgan packages -> nodemon, morgan
+6. API testing with postman
+7. Middleware & types of middleware
+8. Express Error Handling middleware -> body-parser
+9. How to handle HTTP errors-> http-errors
+10. API secure -> xss-clean, express-rate-limit
---
+++ server/src/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const morgan = require('morgan');
+const createError = require('http-errors');
+const bodyParser = require('body-parser');
+//const xssClean = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
+const app = express();
+
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 5, // limit each IP to 5 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 1 minute'
+});
+
+//app.use(xssClean());
+app.use(morgan('dev'));
+app.use(rateLimiter);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+app.get('/test', (req, res) => {
+  res.status(200).send({ message: 'api is working fine! ', });
+});
+
+app.get('/api/user', (req, res) => {
+  res.status(200).send({ message: 'user profile is returned', });
+});
+
+//client error handling
+app.use((req, res, next) => {
+  next(createError(404, 'Route not found'));
+});
+
+
+//server error handling -> all the errors
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({ success: false, message: err.message, });
+});
+
+module.exports = app;
